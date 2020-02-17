115b1ed2e403f370b4cfc1dbfc005b955571da36
devi <arudaladevidivyasri@gmail.com>
2020-02-17 21:36:13 +0530

    week 4

---
+++ src/main/java/com/epam/epamweek4/calculateinterest.java
@@ -0,0 +1,24 @@
+package com.epam.epamweek4;
+import java.util.Scanner;
+public class calculateinterest
+{
+		public static float calculate_si(float principal_amt,float rate,float simple_interest,int time)
+		{
+			return (principal_amt*rate*simple_interest)/time;
+		}
+		public static float calculate_ci(float principal_amt,float rate,float simple_interest,int time)
+		{
+			float ci=principal_amt*((float)Math.pow((1+rate/100),time));
+			return ci;
+		}
+		public static String readInput()
+		{
+			Scanner input=new Scanner(System.in);
+			String str=input.nextLine();
+			input.close();
+			return str;
+		}
+		public static void display(float f){
+		System.out.println(f);
+		}
+}
